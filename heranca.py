b4d91e2f488d41171ba83d0546f7e64afd7dc89b
NasserSalim1 <nasser.salim.beserra@gmail.com>
2025-04-25 09:37:57 -0300

    Adicionando inset e conexões com o Supabase

--- heranca.py
+++ heranca.py
@@ -1,4 +1,11 @@
 from Banco import Banco
+from supabase import create_client
+# from supabase import create_client, Client
+
+url = 'https://dirdlupkocponlqfiulq.supabase.co'
+key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpcmRsdXBrb2Nwb25scWZpdWxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0Mzc3NDAsImV4cCI6MjA2MTAxMzc0MH0.vbNrQAzL7rMsLDCJIUyRm1cpw_ZwmXUqzOiLFwX6Lho'
+
+supabase = create_client(url, key)
 
 class heranca(object):
     def __init__(self, idgastos=0, tipo="", tipo_pgto="", valor="", descrição="", xview="", yview=""):
@@ -15,14 +22,25 @@ class heranca(object):
     def inseregasto(self):
         banco = Banco()
         try:
+            # Insere no banco local
             c = banco.conexao.cursor()
             c.execute("INSERT INTO gastos (tipo, tipo_pgto, valor, descrição) VALUES (?, ?, ?, ?)",
                       (self.tipo, self.tipo_pgto, self.valor, self.descrição))
             banco.conexao.commit()
-            return "Cadastro realizado!"
+
+            # Insere no Supabase
+            supabase.table('gastos').insert({
+                # Inserção sem o campo 'idgastos' porque ele é autoincrementado
+                'tipo': self.tipo,
+                'tipo_pgto': self.tipo_pgto,
+                'valor': self.valor,
+                'descrição': self.descrição
+            }).execute()
+
+            return "Cadastro realizado com sucesso!"
         except Exception as e:
             return f"Erro de cadastro: {str(e)}"
-
+        
     # Atualizar gasto
     def alteragasto(self):
         banco = Banco()
@@ -34,10 +52,12 @@ class heranca(object):
                 WHERE idgastos = ?
             """, (self.tipo, self.tipo_pgto, self.valor, self.descrição, self.idgastos))
             banco.conexao.commit()
-            c.close()
-            return "Gasto atualizado!"
+        
+        # Atualizar no Supabase
+
+            return "Gasto atualizado com sucesso!"
         except Exception as e:
-            return f"Erro na atualização: {str(e)}"
+            return f"Erro ao atualizar gasto: {str(e)}"
 
     # Deletar gasto
     def deletagasto(self):
@@ -46,10 +66,12 @@ class heranca(object):
             c = banco.conexao.cursor()
             c.execute("DELETE FROM gastos WHERE idgastos = ?", (self.idgastos,))
             banco.conexao.commit()
-            c.close()
-            return "Gasto deletado!"
+        
+            # Deletar no Supabase
+
+            return "Gasto deletado com sucesso!"
         except Exception as e:
-            return f"O gasto não foi deletado: {str(e)}"
+            return f"Erro ao deletar gasto: {str(e)}"    
 
     # Consultar gasto
     def consultaGasto(self):
